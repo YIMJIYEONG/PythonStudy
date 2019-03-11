fcefca547efead75a370485890be64abd71992ba
YIMJIYEONG <z0story@naver.com>
2019-03-11 09:09:52 +0900

    숫자 입력받아 정렬하기 - 정렬함수생성 - 출력함수생성
    Signed-off-by: YIMJIYEONG <z0story@naver.com>

--- ArrNum.py
+++ ArrNum.py
@@ -4,6 +4,8 @@ class Arr:
     num3 = 0
     tmp = 0
 
+# 초기화
+
     def __init__(self, num1, num2, num3):
         self.num1 = num1
         self.num2 = num2
@@ -15,24 +17,21 @@ class Arr:
         a = [self.num1, self.num2, self.num3]
         for i in range(0, len(a)-1):
             for j in range(i+1, len(a)):
-                if j < len(a):
-                    if j < 3 and a[i] < a[j]:
-                        self.tmp = a[j]
-                        a[j] = a[i]
-                        a[i] = self.tmp
+                if j < 3 and a[i] < a[j]:
+                    self.tmp = a[j]
+                    a[j] = a[i]
+                    a[i] = self.tmp
         self.num1 = a[0]
         self.num2 = a[1]
         self.num3 = a[2]
 
 # 출력함수
 
-    def print(self):
-        print("num1", "num2", "num3")
-        print(self.num1, self.num2, self.num3)
+    def print_result(self):
+        print("%d > %d > %d" % (self.num1, self.num2, self.num3))
 
 
-arr = Arr(-8, 100, 7)
+arr = Arr(80, 100, 7)
 arr.arrange_num()
-
-print(arr.print())
+arr.print_result()
 
